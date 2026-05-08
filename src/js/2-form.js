70128edff0892072950de9b4425aa34ac96e703d
Novokhatko Ivan <kentvanek80@gmail.com>
2026-05-08 18:39:15 +0300

    hw09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,68 @@
-console.log('Form')
+// Form Data
+const formData = {
+    email: "",
+    message: "",
+};
+
+
+
+// Variables 
+const feedbackForm = document.querySelector(".feedback-form");
+const email = feedbackForm.elements.email;
+const message = feedbackForm.elements.message;
+
+
+
+// Input Listener
+const onFeedbackFormInput = (event) => { 
+    formData.email = email.value.trim();
+    formData.message = message.value.trim();
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+
+feedbackForm.addEventListener('input', onFeedbackFormInput);
+
+
+
+// Content Loaded Listener
+const onWinwodDOMContentLoaded = (event) => {
+    if (localStorage.getItem("feedback-form-state") === null) {
+        return
+    }
+
+    const savedData = localStorage.getItem("feedback-form-state");
+    const parsedData = JSON.parse(savedData);
+
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+
+    email.value = parsedData.email;
+    message.value = parsedData.message;
+
+    console.log(formData)
+};
+
+document.addEventListener('DOMContentLoaded', onWinwodDOMContentLoaded);
+
+
+
+// Submit Listener
+const onFeedbackFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (message.value.trim() === "" || email.value.trim() === "") {
+        return alert("Fill please all fields")
+    } else {
+        console.log(formData);
+
+        localStorage.removeItem("feedback-form-state");
+
+        formData.email = "";
+        formData.message = "";
+
+        feedbackForm.reset();
+    }
+};
+
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
